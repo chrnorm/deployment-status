c1f3112203cbdbd795455c795ffb168619ee5595
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2022-05-22 07:05:57 +0800

    update dependencies and actions tools (#35)
    * update dependencies and actions tools
    
    * add lint config + update dist
    
    * CI: update test workflow
    
    * fix target-url var
    
    * CI: fix environment-url
    
    * CI: bump version
    
    * CI: update tag

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,47 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 type DeploymentState =
-  | "error"
-  | "failure"
-  | "inactive"
-  | "in_progress"
-  | "queued"
-  | "pending"
-  | "success";
+  | 'error'
+  | 'failure'
+  | 'inactive'
+  | 'in_progress'
+  | 'queued'
+  | 'pending'
+  | 'success'
 
-async function run() {
+async function run(): Promise<void> {
   try {
-    const context = github.context;
-    const defaultLogUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
+    const context = github.context
+    const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`
 
-    const token = core.getInput("token", { required: true });
-    const logUrl =
-      core.getInput("log_url", { required: false }) ||
-      core.getInput("target_url", { required: false }) ||
-      defaultLogUrl;
-    const description = core.getInput("description", { required: false }) || "";
-    const deploymentId = core.getInput("deployment_id");
+    const token = core.getInput('token', {required: true})
+
+    const baseUrl =
+      core.getInput('github-base-url', {required: false}) || undefined
+
+    const octokit = github.getOctokit(token, {baseUrl})
+
+    const logUrl = core.getInput('log_url', {required: false}) || defaultUrl
+    const description = core.getInput('description', {required: false}) || ''
+    const deploymentId = core.getInput('deployment_id')
     const environmentUrl =
-      core.getInput("environment_url", { required: false }) || "";
-    const state = core.getInput("state") as DeploymentState;
+      core.getInput('environment_url', {required: false}) || ''
 
-    const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
+    const state = core.getInput('state') as DeploymentState
 
-    await client.repos.createDeploymentStatus({
+    await octokit.rest.repos.createDeploymentStatus({
       ...context.repo,
       deployment_id: parseInt(deploymentId),
       state,
       log_url: logUrl,
       description,
-      environment_url: environmentUrl,
-    });
-  } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
+      environment_url: environmentUrl
+    })
+  } catch (error: any) {
+    core.error(error)
+    core.setFailed(`Error setting GitHub deployment status: ${error.message}`)
   }
 }
 
-run();
+run()
